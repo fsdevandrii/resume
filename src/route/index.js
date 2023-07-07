e7aba613af47bc4333b47510cafab9eaa5fe25fc
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2023-07-07 21:55:33 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяць',
+  address: 'London EC2A 4HJ, United Kingdom',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'dmytro@mail.com',
+      href: 'mailto:dmytro@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+
+// ================================================================
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -15,6 +44,62 @@ router.get('/', function (req, res) {
   res.render('index', {})
   //                  ↑↑ сюди вводимо JSON дані
 })
+// ================================================================
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume  Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'École 42 (France)',
+          isEnd: true,
+        },
+        {
+          name: 'CODE University of Applied Sciences (Germany)',
+          isEnd: true,
+        },
+        {
+          name: 'Codeworks (Spain)',
+          isEnd: true,
+        },
+        {
+          name: 'Makers Academy (United Kingdom)',
+          isEnd: false,
+        },
+        {
+          name: 'Hacktiv8 (Netherlands)',
+          isEnd: false,
+        },
+      ],
+
+      certificates: [
+        {
+          name: 'Gold CE',
+          id: 9001,
+        },
+        {
+          name: 'Silver CE',
+          id: 14001,
+        },
+        {
+          name: 'Grey CE',
+          id: 17025,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 
 // ================================================================
 
@@ -23,10 +108,174 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Sumary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+      },
+
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics ( tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+      },
+    },
+
+    footer,
   })
 })
 
 // ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Handlebars',
+          point: 9,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'Git',
+          point: 7,
+          isTop: false,
+        },
+        {
+          name: 'Terminal',
+          point: 7,
+          isTop: true,
+        },
+        {
+          name: 'NPM',
+          point: 7,
+          isTop: false,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'GYM',
+          isMain: true,
+        },
+        {
+          name: 'Road running',
+          isMain: true,
+        },
+        {
+          name: 'Swimming',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Jonior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmount: 3,
+
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua',
+              about:
+                'Now, with the “internet of things,” nearly everything needs some sort of attention to the user experience.',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Background verification - is a future that provides users to prove that thay are real persons.',
+                },
+                {
+                  name: 'Preparing SEO optimbzed pages. The automated process of getting data for the app from documents.',
+                },
+              ],
+              stackAmount: {
+                point: 3,
+              },
+              awardAmount: {
+                point: 7,
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
